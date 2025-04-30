f27b3daa7c70185ba12a260dfc335eeb807cc729
Michael Marusyk <misha.marusyk@gmail.com>
2025-04-30 20:06:05 +0200

    Add rack-attack gem

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,14 @@
+class Rack::Attack
+  throttle('req/ip', limit: 5, period: 1.second) do |req|
+    req.ip
+  end
+
+  self.throttled_response = lambda do |env|
+    [429, { 
+      'Content-Type' => 'application/json',
+      'Retry-After' => (env['rack.attack.match_data'] || {})[:period].to_s
+    }, [{
+      error: 'Rate limit exceeded. Try again later.'
+    }.to_json]]
+  end
+end
